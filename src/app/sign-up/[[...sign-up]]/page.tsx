cf03ba0f19b54bb643a7f5eb1b88c914d0af5dda
Furkan Cengiz <muhammedcengiz1@gmail.com>
2024-07-21 21:04:22 +0300

    fix: signup page theme and placement.

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,15 @@
+"use client"
 import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function Page() {
-    return <SignUp />;
+    const { theme } = useTheme();
+    return (
+        <main className="grid place-items-center w-full h-full my-auto p-4 sm:p-0">
+            <SignUp appearance={{
+                baseTheme: theme === "dark" ? dark : undefined,
+            }} />
+        </main>
+    )
 }
