1ba75453fe2489318246fd5b391f54492cf35c45
Furkan Cengiz <muhammedcengiz1@gmail.com>
2024-07-22 03:39:48 +0300

    refactor: documentation for SyncActiveOraganizations

--- src/components/SyncActiveOrganizations.tsx
+++ src/components/SyncActiveOrganizations.tsx
@@ -2,6 +2,12 @@
 import { useEffect } from "react";
 import { useAuth, useOrganizationList } from "@clerk/nextjs";
 
+
+/**
+ * @brief This function is used to sync the active organization of the user with the first organization in the membership
+ * @param membership is the membership of the user in the organizations, the key is the organization ID and the value is the role of the user in the organization
+ * @returns null, it doesn't return anything. It just syncs the active organization with the first organization in the membership
+ */
 export function SyncActiveOrganization({
     membership,
 }: {
